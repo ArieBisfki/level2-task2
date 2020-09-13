4c309c2a6012839507709dad8f3ac742782a9768
ArieBisfki <arie_bisfki@live.nl>
2020-09-13 16:31:52 +0200

    Updated questions. Assignment complete?

--- app/src/main/java/com/example/madlevel2task2/Question.kt
+++ app/src/main/java/com/example/madlevel2task2/Question.kt
@@ -7,7 +7,9 @@ data class Question(
     companion object {
         val list = arrayListOf(
             Question("A 'val' and 'var' are the same.", false),
-            Question("Mobile Application Development grants 12 ECTS.", true)
+            Question("Mobile Application Development grants 12 ECTS.", false),
+            Question("A Unit in Kotlin corresponds to a void in Java.", true),
+            Question("In Kotlin 'when' replaces the 'switch' operator in Java.", true)
         )
     }
 }
